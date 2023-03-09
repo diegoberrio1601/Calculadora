97d3b7a4e8d01ac3f0da8fe70ec34e26510dd6b2
diegoberrio1601 <diegoberrio1601@gmail.com>
2023-03-09 13:59:14 -0500

    first commit

--- calculadora/ViewController.swift
+++ calculadora/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var numeroUnoTextField: UITextField!
+    @IBOutlet var numeroDosTextField: UITextField!
+    @IBOutlet var resultadoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func accionSumar(_ sender: UIButton) {
+        let numeroUno = Int(numeroUnoTextField.text!)!
+        let numeroDos = Int(numeroDosTextField.text!)!
+        let resultado = numeroUno + numeroDos
+        
+        resultadoLabel.text = "\(resultado)"
+    }
+    
+    @IBAction func accionRestar(_ sender: UIButton) {
+        let numeroUno = Int(numeroUnoTextField.text!)!
+        let numeroDos = Int(numeroDosTextField.text!)!
+        let resultado = numeroUno - numeroDos
+        
+        resultadoLabel.text = "\(resultado)"
+    }
+    @IBAction func accionMultiplicar(_ sender: UIButton) {
+        let numeroUno = Int(numeroUnoTextField.text!)!
+        let numeroDos = Int(numeroDosTextField.text!)!
+        let resultado = numeroUno * numeroDos
+        
+        resultadoLabel.text = "\(resultado)"
+    }
+    @IBAction func accionDividir(_ sender: UIButton) {
+        let numeroUno = Int(numeroUnoTextField.text!)!
+        let numeroDos = Int(numeroDosTextField.text!)!
+        let resultado = numeroUno / numeroDos
+        
+        resultadoLabel.text = "\(resultado)"
+    }
+    
 }
 
